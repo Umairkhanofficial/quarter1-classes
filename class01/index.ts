fc694124c5ba37751cd496e8ef52d49a03890ec1
Muhammad Umair <142714604+Umairkhanofficial@users.noreply.github.com>
2023-10-13 22:53:28 +0500

    Update index.ts

--- class01/index.ts
+++ class01/index.ts
@@ -7,17 +7,3 @@ var FullName = FirstName + LastName;
 console.log(FullName);
 console.log(age,contact);
 
-let car:string = "Bmw";
-console.log("car name ",car)
-
-console.log("First Program")     
-var FirstN:string = "Umair";
-var LastN:string = "Khan";
-var age:n = 25;
-var contact:number =3006647517;   
-var FullName = FirstName + LastName;
-console.log(FullName);
-console.log(age,contact);
-
-let car:string = "Bmw";
-console.log("car name ",car)
