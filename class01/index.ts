7c20f9e11b391f0f531ecfc0e88c8b252f290993
Muhammad Umair <142714604+Umairkhanofficial@users.noreply.github.com>
2023-10-17 23:36:14 +0500

    Update index.ts

--- class01/index.ts
+++ class01/index.ts
@@ -15,3 +15,9 @@ function addNumbers(a: number, b: number): number {
 
 const result = addNumbers(5, 7);
 console.log(`The result is: ${result}`);
+
+const array: number[] = Array(10) // array size is 10
+  .fill(undefined)
+  .map(() => 50 * Math.random()); // numbers from 0-50 (exclusive)
+
+console.log(array);
