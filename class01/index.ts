6a1866f556c175535b1c351f6e915d57b1d6ff64
Muhammad Umair <142714604+Umairkhanofficial@users.noreply.github.com>
2023-10-18 23:41:53 +0500

    Update index.ts

--- class01/index.ts
+++ class01/index.ts
@@ -21,3 +21,17 @@ const array: number[] = Array(10) // array size is 10
   .map(() => 50 * Math.random()); // numbers from 0-50 (exclusive)
 
 console.log(array);
+
+//factorial code
+function factorial(n: number): number {
+    if (n <= 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+const num = 5;
+const result = factorial(num);
+
+console.log(`The factorial of ${num} is: ${result}`);
